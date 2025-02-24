ce96b45ba8b0b06526af89d14267a9a4575987ae
Rafał Zaborowski <raffik2000@gmail.com>
2025-02-24 21:45:37 +0100

    add status bar

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
 import CategoriesScreen from "./screens/CategoriesScreen";
 
 export default function App() {
-  return (
-    <CategoriesScreen />
-  );
+    return (
+        <>
+            <StatusBar style="light-content"/>
+            <CategoriesScreen/>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-  },
+    container: {},
 });
