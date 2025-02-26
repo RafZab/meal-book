59ca8287905c27ca6defaf98ad6d738b06389094
Rafał Zaborowski <raffik2000@gmail.com>
2025-02-26 22:25:17 +0100

    show meals

---
+++ components/MealItem.jsx
@@ -0,0 +1,67 @@
+import {StyleSheet, Text, View, Platform, Pressable, Image} from 'react-native';
+import {useNavigation} from "@react-navigation/native";
+
+const MealItem = ({title, imageUrl, duration, complexity, affordability}) => {
+    return (
+        <View style={styles.mealItem}>
+            <Pressable android_ripple={{color: '#ccc'}}
+                       style={({pressed}) => (pressed ? styles.buttonPressed : null)}>
+                <View style={styles.interContainer}>
+                    <View>
+                        <Image source={{uri: imageUrl}} style={styles.image}/>
+                        <Text style={styles.title}>{title}</Text>
+                    </View>
+                    <View style={styles.details}>
+                        <Text style={styles.detailsItem}>{duration}m</Text>
+                        <Text style={styles.detailsItem}>{complexity.toUpperCase()}</Text>
+                        <Text style={styles.detailsItem}>{affordability.toUpperCase()}</Text>
+                    </View>
+                </View>
+            </Pressable>
+        </View>
+    )
+}
+
+export default MealItem
+
+const styles = StyleSheet.create({
+    mealItem: {
+        margin: 16,
+        borderRadius: 8,
+        backgroundColor: 'white',
+        elevation: 4,
+        borderColor: "#fff", // without background shadow not working!
+        shadowColor: "#000",
+        shadowOpacity: 0.35,
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 16,
+        overflow: Platform.OS === "ios" ? "visible" : "hidden",
+    },
+    interContainer: {
+        borderRadius: 8,
+        overflow: "hidden",
+    },
+    buttonPressed: {
+        opacity: 0.5
+    },
+    image: {
+        width: '100%',
+        height: 200,
+    },
+    title: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: 18,
+        margin: 8
+    },
+    details: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 8
+    },
+    detailsItem: {
+        marginHorizontal: 4,
+        fontSize: 12,
+    }
+})
--- screens/MealsOverviewScreen.jsx
+++ screens/MealsOverviewScreen.jsx
@@ -1,16 +1,26 @@
 import {MEALS} from "../data/dummy-data";
-import {View, Text, StyleSheet} from "react-native";
+import {View, Text, StyleSheet, FlatList} from "react-native";
 import {useRoute} from "@react-navigation/native";
+import MealItem from "../components/MealItem";
 
 const MealsOverviewScreen = ({route}) => {
     const catId = route.params.categoryId; // or const route = useRoute()
+    const displayedMeals = MEALS.filter((mealItem) => mealItem.categoryIds.indexOf(catId) >= 0);
 
+    const renderMealItem = (mealData) => {
+        const mealItemProps = {
+            title: mealData.item.title,
+            imageUrl: mealData.item.imageUrl,
+            duration: mealData.item.duration,
+            complexity: mealData.item.complexity,
+            affordability: mealData.item.affordability,
+        }
+        return <MealItem {...mealItemProps} />
+    }
 
     return (
         <View style={styles.container}>
-            <Text>
-                Meals Overview Screen - {catId}
-            </Text>
+            <FlatList data={displayedMeals} keyExtractor={(item) => item.id} renderItem={renderMealItem}/>
         </View>
     )
 }
