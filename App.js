355548dcb16b80d496a6f3e11bb879be59a7c365
Rafał Zaborowski <raffik2000@gmail.com>
2025-02-27 23:16:52 +0100

    styled stack navigator

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import {StyleSheet, Text, View} from 'react-native';
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,9 +13,15 @@ export default function App() {
         <>
             <StatusBar style="dark"/>
             <NavigationContainer>
-                <Stack.Navigator initialRouteName="MealsCategories">
-                    <Stack.Screen name={"MealsCategories"} component={CategoriesScreen} />
-                    <Stack.Screen name={"MealsOverview"} component={MealsOverviewScreen} />
+                <Stack.Navigator initialRouteName="MealsCategories" screenOptions={{
+                    headerStyle: {backgroundColor: '#934c4c'},
+                    headerTintColor: 'white',
+                    contentStyle: {backgroundColor: '#934c4c'}
+                }}>
+                    <Stack.Screen name={"MealsCategories"} component={CategoriesScreen} options={{
+                        title: 'All Categories'
+                    }}/>
+                    <Stack.Screen name={"MealsOverview"} component={MealsOverviewScreen}/>
                 </Stack.Navigator>
             </NavigationContainer>
         </>
